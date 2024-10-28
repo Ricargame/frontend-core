69f0aeeae2507259cc02ca3e8efed2bb9adf7df4
Ricargame <ricardofenomeno13@gmail.com>
2024-10-28 09:36:30 -0400

    Fix: Keep Record In Window

--- src/store/modules/ADempiere/windowManager.js
+++ src/store/modules/ADempiere/windowManager.js
@@ -91,7 +91,9 @@ const windowManager = {
       isLoading = false,
       pageNumber = 1,
       pageSize = ROWS_OF_RECORDS_BY_PAGE_HIGH,
-      sortBy
+      sortBy,
+      referenceUuid,
+      contextAttributes
     }) {
       const dataTab = {
         parentUuid,
@@ -107,7 +109,9 @@ const windowManager = {
         isLoading,
         pageNumber,
         pageSize,
-        sortBy
+        sortBy,
+        referenceUuid,
+        contextAttributes
       }
       Vue.set(state.tabData, containerUuid, dataTab)
       Vue.set(state.oldTabData, containerUuid, dataTab)
@@ -299,6 +303,12 @@ const windowManager = {
       pageSize = ROWS_OF_RECORDS_BY_PAGE_HIGH,
       sortBy
     }) {
+      if (isEmptyValue(filters)) {
+        filters = rootGetters.getTabDataFilters({
+          parentUuid,
+          containerUuid
+        })
+      }
       return new Promise(resolve => {
         const {
           isParentTab,
@@ -310,7 +320,6 @@ const windowManager = {
           parent_column_name,
           context_column_names
         } = rootGetters.getStoredTab(parentUuid, containerUuid)
-
         if (!isEmptyValue(filters) && typeof filters !== 'object') {
           const parseFilter = JSON.parse(filters)
           filters = [parseFilter]
@@ -365,7 +374,6 @@ const windowManager = {
           keyName: 'key',
           format: 'object'
         })
-
         // const isWithoutValues = context_column_names.find(columnName =>
         //   isEmptyValue(columnName) ||
         //   isEmptyValue(contextAttributesList[columnName])
@@ -408,7 +416,8 @@ const windowManager = {
           isLoaded: false,
           containerUuid,
           pageSize,
-          sortBy
+          sortBy,
+          referenceUuid
         })
         commit('setIsLoadingTabRecordsList', {
           containerUuid,
@@ -461,7 +470,6 @@ const windowManager = {
         if (!isEmptyValue(contextAttributesList)) {
           contextAttributes = JSON.stringify(contextAttributesList)
         }
-
         let listFilters
         if (!isEmptyValue(filters)) {
           listFilters = '[' + filters.map(parameter => {
@@ -480,7 +488,17 @@ const windowManager = {
             })
           }).toString() + ']'
         }
-
+        if (!isEmptyValue(contextAttributes)) {
+          commit('setTabData', {
+            containerUuid,
+            contextAttributes
+          })
+        }
+        if (isEmptyValue(contextAttributes)) {
+          contextAttributes = getters.getTabData({
+            containerUuid
+          }).contextAttributes
+        }
         requestGetEntities({
           tabId: internal_id,
           contextAttributes,
@@ -614,7 +632,9 @@ const windowManager = {
               isLoaded: true,
               isLoading: false,
               recordCount: dataResponse.recordCount,
-              sortBy
+              sortBy,
+              referenceUuid,
+              contextAttributes
             })
 
             if (isEmptyValue(dataToStored)) {
@@ -648,7 +668,9 @@ const windowManager = {
             commit('setTabData', {
               parentUuid,
               isLoaded: true,
-              containerUuid
+              containerUuid,
+              referenceUuid,
+              contextAttributes
             })
             commit('setIsLoadingTabRecordsList', {
               containerUuid,
@@ -941,7 +963,8 @@ const windowManager = {
         selectionsList,
         currentRecordUuid,
         pageNumber,
-        pageSize
+        pageSize,
+        referenceUuid
       } = getters.getTabData({
         containerUuid
       })
@@ -955,7 +978,8 @@ const windowManager = {
         currentRecordUuid,
         pageNumber,
         isLoaded,
-        pageSize
+        pageSize,
+        referenceUuid
       })
     }
   },
