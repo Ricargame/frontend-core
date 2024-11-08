517325bd30acb528fa630f59011036f2f0085326
Edwin Betancourt <EdwinBetanc0urt@outlook.com>
2024-11-08 15:17:03 -0400

    Update references.js

--- src/utils/ADempiere/references.js
+++ src/utils/ADempiere/references.js
@@ -572,11 +572,11 @@ export const FIELDS_AMOUNT = [
 ]
 
 export const FIELDS_QUANTITY = [
+  AMOUNT.id,
   COSTS_PLUS_PRICES.id,
   INTEGER.id,
   NUMBER.id,
-  QUANTITY.id,
-  AMOUNT.id
+  QUANTITY.id
 ]
 
 /**
